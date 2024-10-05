3e8f09132e831f9a23b1d55761daf1c5fe46aac7
Mayur0018 <mayurnish18@gmail.com>
2024-10-05 16:02:27 +0530

    commit

--- src/Pages/SignUp.jsx
+++ src/Pages/SignUp.jsx
@@ -2,8 +2,19 @@ import Header from "../Components/Header";
 import Footer from "../Components/Footer";
 import SideImg from "../assets/SignUp.png";
 import { FcGoogle } from "react-icons/fc";
+import { Link } from "react-router-dom";
+import { useState } from "react";
 
 export default function SignUp() {
+  const [data , setData] = useState({
+    name: "",
+    number : "",
+    password : "",
+  });
+
+  const handleStoreData = (e)=>{
+    setData(e.target.value)
+  }
   return (
     <>
       <Header />
@@ -18,7 +29,7 @@ export default function SignUp() {
             <div className="w-full">
               <div className="text-center">
                 <h1 className="text-3xl font-semibold text-gray-900">
-                Create an account
+                  Create an account
                 </h1>
                 <p className="mt-2 text-gray-500">
                   Sign in below to access your account
@@ -31,8 +42,10 @@ export default function SignUp() {
                       type="Name"
                       name="Name"
                       id="Name"
+                      value={data}
                       placeholder="Name"
-                      className="peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500 focus:outline-none" 
+                      onChange={handleStoreData}
+                      className="peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
                     />
                     <label
                       htmlFor="email"
@@ -46,14 +59,15 @@ export default function SignUp() {
                       type="number"
                       name="Email or Phone Number"
                       id="Email or Phone Number"
+                      value={data}
                       placeholder="Email or Phone Number"
                       className="peer peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
-                    />
+                     />
                     <label
                       htmlFor="Email or Phone Number"
                       className="pointer-events-none absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800"
                     >
-                    Email or Phone Number
+                      Email or Phone Number
                     </label>
                   </div>
                   <div className="relative mt-6">
@@ -61,6 +75,7 @@ export default function SignUp() {
                       type="password"
                       name="Password"
                       id="Password"
+                      value={data}
                       placeholder="Password"
                       className="peer peer mt-1 w-full border-b-2 border-gray-300 px-0 py-1 placeholder:text-transparent focus:border-gray-500 focus:outline-none"
                     />
@@ -68,7 +83,7 @@ export default function SignUp() {
                       htmlFor="Password"
                       className="pointer-events-none absolute top-0 left-0 origin-left -translate-y-1/2 transform text-sm text-gray-800 opacity-75 transition-all duration-100 ease-in-out peer-placeholder-shown:top-1/2 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-focus:top-0 peer-focus:pl-0 peer-focus:text-sm peer-focus:text-gray-800"
                     >
-                   Passwords
+                      Passwords
                     </label>
                   </div>
                   <div className="mt-6">
@@ -76,7 +91,7 @@ export default function SignUp() {
                       type="submit"
                       className="w-full rounded-md bg-customRed px-3 py-4 text-white focus:bg-gray-600 focus:outline-none"
                     >
-       Create Account
+                      Create Account
                     </button>
                   </div>
                   <div className="mt-4">
@@ -84,18 +99,18 @@ export default function SignUp() {
                       type="submit"
                       className="w-full rounded-md bg-customWhite pb-4 text-customBlack focus:bg-gray-600 focus:outline-none"
                     >
-                      <FcGoogle className=" text-2xl relative top-6 left-8"/>
-     Sign up with Google
+                      <FcGoogle className=" text-2xl relative top-6 left-8" />
+                      Sign up with Google
                     </button>
                   </div>
                   <p className="text-center text-sm text-gray-500">
-                  Already have account?
-                    <a
-                      href="#!"
+                    Already have account?
+                    <Link
+                      to="/Login"
                       className="font-semibold text-gray-600 hover:underline focus:text-gray-800 focus:outline-none"
                     >
                       Log in
-                    </a>
+                    </Link>
                     .
                   </p>
                 </form>
